c10ea659d3ce7684b2179f48fd546450ec3e64f1
ZarinaMambetova <zarina.mambetova84@gmail.com>
2020-09-19 04:20:40 +0300

    убрала закрытые данные админки слизы

--- js/script.js
+++ js/script.js
@@ -4,15 +4,19 @@
   const result = document.querySelector('#result');
 
   // Функция для перенаправления на главную страницу слизы для тех, у кого нет доступа к модулю:
-  function redirect() {
-    window.location.replace('https://sliza.ru');
-  }
+  // function redirect() {
+  //   window.location.replace('https://sliza.ru');
+  // }
+  // function zapret()
+  // {
+  //    var btn = document.getElementById('sendButton').disabled = true;
+  // }
 
   // Обозначаю, с какого адреса идет запрос, чтобы можно было тестить без основного адреса:
-  let url = '';
-  if (window.location.href.includes('ambetova') || window.location.href.includes(':5500'))
-    url = 'https://zeta2.dev.sliza.ru/api/v1/index.php' + window.location.search;
-  else url = 'https://sliza.ru/api/v1/index.php' + window.location.search;
+  // let url = '';
+  // if (window.location.href.includes('ambetova') || window.location.href.includes(':5500'))
+  //   url = 'https://httpbin.org/post' + window.location.search;
+  
 
   //Обработчик события "клик":
   sendButton.addEventListener('click', function(event) {
@@ -30,22 +34,29 @@
           host: myForm.elements.host.value,
           price: myForm.elements.price.value,
           count: myForm.elements.count.value,
-          rate_id: myForm.elements.rate_id.value
+          rate_id: myForm.elements.rate_id.value,
+          
         }
       };
 
+      const rate_id_title = {
+        label: myForm.rate_id.options[data.params.rate_id].title,
+      }; 
+
+
       const xhr = new XMLHttpRequest();
       xhr.responseType = 'json'; //ожидаем от сервера данные в формате json
-      xhr.open('POST', url, true); // Метод post и куда отправляем данные.
+      xhr.open('POST', 'https://httpbin.org/post'); // Метод post и куда отправляем данные.
       xhr.send(JSON.stringify(data)); // превращаем данные в сроку и отправляем на сервер
       xhr.addEventListener('load', () => {
         // если нет прав, то редирект:
         if (xhr.response.success == false) {
           redirect();
         } else {
+         
           //мультизапись - без удаления предыдущего:
 
-          result.insertAdjacentHTML('beforebegin', "<div class='old_result'>" + result.innerHTML + '</div>');
+          result.insertAdjacentHTML('afterend', "<div class='old_result'>" + result.innerHTML + '</div>');
 
           // вывод необходимых значений по колонкам:
 
@@ -63,11 +74,18 @@
             data.params.rate_id +
             '</div>' +
             '<div>' +
-            xhr.response.result +
+              rate_id_title.label +
+            '</div>' +
+            '<div>' +
+              xhr.response.result + 
             '</div>';
-        }
+          
+        } 
+        
+
         console.log(xhr);
         console.log(data);
+        console.log(rate_id_title);
       });
     }
   });
